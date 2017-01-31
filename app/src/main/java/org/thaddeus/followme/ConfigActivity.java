b5a240e483a2f458fe2743bc9575dda81f74f5d6
thaddeus002 <thaddeus.dupont@free.fr>
2017-01-31 23:06:23 +0100

    Created new app in android studio

---
+++ app/src/main/java/org/thaddeus/followme/ConfigActivity.java
@@ -0,0 +1,13 @@
+package org.thaddeus.followme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+}
