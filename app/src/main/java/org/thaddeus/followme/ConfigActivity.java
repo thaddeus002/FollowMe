eee7261df586f5d32d78d829349f25591ffd4628
thaddeus002 <thaddeus.dupont@free.fr>
2017-02-01 20:48:23 +0100

    Simple interface layout

--- app/src/main/java/org/thaddeus/followme/ConfigActivity.java
+++ app/src/main/java/org/thaddeus/followme/ConfigActivity.java
@@ -2,6 +2,11 @@ package org.thaddeus.followme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 public class ConfigActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class ConfigActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
+        
+        final EditText editServer = (EditText) findViewById(R.id.editServer);
+        
+        Button sendButton = (Button) findViewById(R.id.sendButton);
+        
+        sendButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               String server = editServer.getText().toString();
+               
+               String message = null;
+               if(server!= null && server.length()>0) {
+                   message = "Sended to "+server;
+               } else {
+                   message = "No server defined";
+               }
+               
+               Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+           } 
+        });
     }
 }
